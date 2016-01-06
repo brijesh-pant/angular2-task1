9470b26d702ef2109ab99ecb9274d6c25f4fcaf8
Brijesh Pant <brijesh.pant@daffodilsw.com>
2016-01-06 17:25:50 +0530

    1. Changed Register user page with validations and updation

--- app/users/user-info.component.js
+++ app/users/user-info.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', 'angular2/router', './user.service'], function(exports_1) {
+System.register(['angular2/core', 'angular2/router', 'angular2/common', '../validations/validation-service.component', './user.service'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -8,8 +8,13 @@ System.register(['angular2/core', 'angular2/router', './user.service'], function
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, router_1, router_2, user_service_1;
+    var core_1, router_1, router_2, common_1, validation_service_component_1, user_service_1;
     var UserInfoComponent;
+    function emailValidator(control) {
+        if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+            return { invalidEmail: true };
+        }
+    }
     return {
         setters:[
             function (core_1_1) {
@@ -19,34 +24,45 @@ System.register(['angular2/core', 'angular2/router', './user.service'], function
                 router_1 = router_1_1;
                 router_2 = router_1_1;
             },
+            function (common_1_1) {
+                common_1 = common_1_1;
+            },
+            function (validation_service_component_1_1) {
+                validation_service_component_1 = validation_service_component_1_1;
+            },
             function (user_service_1_1) {
                 user_service_1 = user_service_1_1;
             }],
         execute: function() {
             UserInfoComponent = (function () {
-                function UserInfoComponent(_router, _routeParams, _userService) {
+                function UserInfoComponent(_router, _routeParams, _userService, fb) {
                     this._router = _router;
                     this._routeParams = _routeParams;
                     this._userService = _userService;
+                    this.fb = fb;
                 }
                 UserInfoComponent.prototype.onEdit = function () {
                     this.editUser = true;
                     this.originalUser = this.user;
                 };
-                UserInfoComponent.prototype.onUpdateDetails = function () {
+                UserInfoComponent.prototype.onUpdateDetails = function (user) {
+                    //console.log("user",user);
+                    this.user.name = user.name;
+                    this.user.email = user.email;
+                    this.user.description = user.description;
                     this.editUser = false;
                     this.goToUserInfo();
                 };
                 UserInfoComponent.prototype.onCancel = function () {
                     // cancel not done this.editUser referencing to the same this.user object 
                     // create a new user object from this.user to solve this
-                    this.user = this.originalUser;
                     this.editUser = false;
                     this.goToUserInfo();
                 };
                 UserInfoComponent.prototype.goToUserInfo = function () {
-                    console.log(this.user.id);
+                    console.log("user id", this.user.id);
                     var route = ['UserInfo', { id: this.user.id }];
+                    console.log("route", route);
                     this._router.navigate(route);
                 };
                 UserInfoComponent.prototype.ngOnInit = function () {
@@ -55,14 +71,21 @@ System.register(['angular2/core', 'angular2/router', './user.service'], function
                     var id = this._routeParams.get('id');
                     this._userService.getUser(id).then(function (user) {
                         _this.user = user;
+                        _this.updateUserForm = _this.fb.group({
+                            'name': [_this.user.name, common_1.Validators.required],
+                            'email': [_this.user.email, common_1.Validators.compose([
+                                    common_1.Validators.required, emailValidator])],
+                            'description': [_this.user.description, common_1.Validators.required]
+                        });
                     });
                 };
                 UserInfoComponent = __decorate([
                     core_1.Component({
                         templateUrl: '/views/userInfo.html',
-                        providers: [user_service_1.UserService]
+                        providers: [user_service_1.UserService],
+                        directives: [common_1.FORM_DIRECTIVES, validation_service_component_1.ControlMessagesComponent]
                     }), 
-                    __metadata('design:paramtypes', [router_1.Router, router_2.RouteParams, user_service_1.UserService])
+                    __metadata('design:paramtypes', [router_1.Router, router_2.RouteParams, user_service_1.UserService, common_1.FormBuilder])
                 ], UserInfoComponent);
                 return UserInfoComponent;
             })();
--- app/users/user-info.component.ts
+++ app/users/user-info.component.ts
@@ -2,30 +2,40 @@ import {Component} from 'angular2/core';
 import {OnInit} from 'angular2/core';
 import {Router} from 'angular2/router';
 import {RouteParams} from 'angular2/router';
+import {FORM_DIRECTIVES, FormBuilder, Control, ControlGroup, Validators} from 'angular2/common';
 
+import {ControlMessagesComponent} from '../validations/validation-service.component'
 import {User} from './user';
 import {UserService} from './user.service';
 
 @Component({
 	templateUrl: '/views/userInfo.html',
-	providers: [UserService]
+	providers: [UserService],
+	directives: [FORM_DIRECTIVES, ControlMessagesComponent]
 })
 
 export class UserInfoComponent implements OnInit { 
+	updateUserForm: ControlGroup;
 	public user: User;
 	public originalUser: User;
 	public editUser: boolean;
 	constructor(private _router: Router,
 		private _routeParams: RouteParams,
-		private _userService: UserService){
-	}
-
+		private _userService: UserService,
+		private fb: FormBuilder) {
+			
+		}
+	
 	onEdit(){
 		this.editUser = true;
 		this.originalUser = this.user;
 	}
 
-	onUpdateDetails(){
+	onUpdateDetails(user: User){
+		//console.log("user",user);
+		this.user.name = user.name;
+		this.user.email = user.email;
+		this.user.description = user.description;
 		this.editUser = false;
 		this.goToUserInfo();
 	}
@@ -33,14 +43,14 @@ export class UserInfoComponent implements OnInit {
 	onCancel(){
 		// cancel not done this.editUser referencing to the same this.user object 
 		// create a new user object from this.user to solve this
-		this.user = this.originalUser;
 		this.editUser = false;
 		this.goToUserInfo();
 	}
 
 	goToUserInfo(){
-		console.log(this.user.id);
+		console.log("user id",this.user.id);
 		let route = ['UserInfo', { id: this.user.id }];
+		console.log("route", route);
 		this._router.navigate(route);
 	}
 
@@ -49,7 +59,20 @@ export class UserInfoComponent implements OnInit {
 		let id = this._routeParams.get('id');
 		this._userService.getUser(id).then(user => {
 			this.user = user;
+			this.updateUserForm = this.fb.group({
+				'name': [this.user.name, Validators.required],
+				'email': [this.user.email, Validators.compose([
+					Validators.required, emailValidator])],
+				'description': [this.user.description, Validators.required]
+			});
 		});
+		
+	}
+}
+
+function emailValidator(control: Control): { [s: string]: boolean } {
+	if (!control.value.match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)) {
+		return { invalidEmail: true };
 	}
 }
 
